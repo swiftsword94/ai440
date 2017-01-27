31e8517e519c4cdb076f4ae2ad4e5b064b3e8999
swiftsword94 <swiftsword94@gmail.com>
2017-01-27 16:30:31 -0500

    Added Node.java

---
+++ Node.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList
+
+public class Node
+{
+	public ArrayList<Node> neighbors = null;
+	public Character type = null;
+	public Node parent = null;
+	public Node()
+	{
+	}
+}
